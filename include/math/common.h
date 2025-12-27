6f01394087ab50d088e159949d3893be163183d1
Muyunaaaa <485840404@qq.com>
2025-12-27 23:01:44 +0800

    引入第三方依赖

--- include/math/common.h
+++ include/math/common.h
@@ -10,7 +10,8 @@ namespace myu {
             float x;
             float y;
             float z;
-            Vec3(float x = 0.0f, float y = 0.0f, float z = 0.0f) : x(x), y(y), z(z) {}
+            constexpr Vec3(float x = 0.0f, float y = 0.0f, float z = 0.0f)
+            : x(x), y(y), z(z) {}
         };
     }
 }
